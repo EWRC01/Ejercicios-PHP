faffb9d9e00400f51faf17b4ce54d314e801e891
@Wilfredo <edwin1026@hotmail.es>
2021-03-21 20:16:13 -0600

    Ejercicio 6 completado

---
+++ ejercicio6.php
@@ -0,0 +1,14 @@
+<?php
+
+        declare (strict_types = 1);
+            function mayorDe($n1, $n2) :int {
+
+
+                        return   max($n1, $n2);
+
+            }
+
+
+           echo "<h1>El mayor de los dos numeros es : " . mayorDe(4, 6). "</h1>" ;
+
+?>
